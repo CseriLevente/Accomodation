d08b67d02bbfddb3ab493deba67cda4aa769d8b2
Cséri Levente <cserilevente@gmail.com>
2024-03-05 19:29:42 +0100

    Trying to make the first gets and posts

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
-const app=require('express')();
-const PORT=8081;
+const express=require('express');
+const app=express();
+const PORT=8083;
+
+let resorts = [
+    {id:1, name:"Ocean Hotel", location: "Petőfi Sándor utca 1.", cost: 500}
+]
+
+app.use(express.json())
+
+app.get('api/resorts', (req, res) => {
+    res.json(resorts);
+});
+
+app.get('/api/resorts/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const resort = resorts.find(r => r.id === id);
+    if (!resort) {
+        return res.status(404).json({ message: 'Resort not found' });
+    }
+    res.json(resort);
+});
+
+app.post('/api/resorts', (req, res) => {
+    const { id, name, location, cost } = req.body;
+    const newResort = { id, name, location, cost };
+    resorts.push(newResort);
+    res.status(201).json(newResort);
+});
 
 app.listen(
     PORT,
