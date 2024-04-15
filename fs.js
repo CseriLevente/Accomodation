da17e6c2a45d83705add31a518bdfe2cdaaa16ca
Cséri Levente <cserilevente@gmail.com>
2024-04-15 17:24:47 +0200

    functions

--- fs.js
+++ fs.js
@@ -29,7 +29,9 @@ fs.writeFile("./api.txt", ";Fortnite", { flag: "a" }, (err) => {
   });
 }*/
 
-function findResortById(id, (callback)=>{
+//addResort("Whispering Pines Lodge", "Yosemite", 698);
+
+/*function findResortById(id, (callback)=>{
 callback()
   fs.readFile("./api,txt", "utf-8", (error, data) => {
     if (error) {
@@ -42,7 +44,7 @@ callback()
     }
   return right;
   );
-
+*/
 /*fs.readFile("./api.txt", "utf-8", (error, data) => {
   if (error) {
     console.log(error);
@@ -51,4 +53,65 @@ callback()
   console.log(lines);
 });*/
 
-findResortById("ba52a8f8-76b8-4324-a0d6-fbf6af21fa94");
+/*function findResortById(id, callback) {
+  fs.readFile("./api.txt", "utf-8", (error, data) => {
+    if (error) {
+      console.log(error);
+    } else {
+      const lines = data.split("\n");
+      const foundLine = lines.find((line) => line.includes(id));
+      callback(foundLine);
+    }
+  });
+}*/
+
+/*findResortById("ba52a8f8-76b8-4324-a0d6-fbf6af21fa94", (error, result) => {
+  if (error) {
+    console.log(error);
+  }
+  console.log(result);
+});*/
+
+/*function deleteResortById(id, callback) {
+  fs.readFile("./api.txt", "utf-8", (error, data) => {
+    if (error) {
+      console.log(error);
+      callback(error);
+    } else {
+      const lines = data.split("\n");
+      const foundIndex = lines.findIndex((line) => line.includes(id));
+      if (foundIndex !== -1) {
+        lines.splice(foundIndex, 1);
+        const updatedData = lines.join("\n");
+        fs.writeFile("./api.txt", updatedData, "utf-8", (err) => {
+          if (err) {
+            console.log(err);
+            callback(err);
+          } else {
+            callback(null, "Succesful delete!");
+          }
+        });
+      } else {
+        callback("ID not found");
+      }
+    }
+  });
+}
+
+deleteResortById("81a46927-de23-4897-85f8-e07f47f184a9", (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
+});
+*/
+
+fs.readFile("./api.txt", "utf-8", (error, data) => {
+  if (error) {
+    console.log(error);
+    callback(error);
+  } else {
+    const lines = data.split("\n");
+    console.log(lines);
+  }
+});
